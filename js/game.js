f27c529ecdaa2332b6e7e481f89e1ce986f0bc24
jgpentz <jpentz1@gmail.com>
2019-12-15 20:14:31 -0700

    added js

---
+++ js/game.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height:800,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
